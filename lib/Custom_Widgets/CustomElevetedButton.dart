1d38a5bdd3d046d9ccff9c7f7d628bf27f77d0d8
harinandan97 <hariharinandan1@gmail.com>
2025-12-06 13:24:43 +0530

    commit

---
+++ lib/Custom_Widgets/CustomElevetedButton.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+
+  final String txt;
+  final Color? clr;
+
+  const CustomButton({required this.txt,this.clr});
+
+  @override
+  Widget build(BuildContext context) {
+    return 
+      ElevatedButton(onPressed: (){},
+          style:ElevatedButton.styleFrom(
+            backgroundColor:clr?? const Color(0xFF758F8E), // grey-green shade,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(50)),maximumSize:const Size(double.infinity, 55),elevation: 2 )
+          , child:
+      Text(txt!,style: TextStyle(  color: Colors.black, // text color
+        fontSize: 16,
+        fontWeight: FontWeight.w500,),)
+      )
+      ;
+  }
+}
